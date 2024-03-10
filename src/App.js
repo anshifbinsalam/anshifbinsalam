88098605071609ee9f9a69693fde2240aa2e9d61
anshifbinsalam <anshifbinsalamk@gmail.com>
2024-03-10 19:24:37 +0530

    link

--- src/App.js
+++ src/App.js
@@ -2,40 +2,40 @@
 import "./App.css"
 import '@fortawesome/fontawesome-free/css/all.css';
 
-
+// App.js
 import React from "react";
-// import Navbar from "./components/Navbar";
-// import {
-// 	BrowserRouter as 
-// 	Routes,
-// 	Route,
-// } from "react-router-dom";
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-} from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/index";
 import About from "./Pages/about";
 import Project from "./Pages/project";
 import Contact from "./Pages/contact";
 
 function App() {
-	return (
-		<div className='App'>
-		<Router basename="/">
-			<Routes>
-				<Route exact path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route
-					path="/contact"
-					element={<Contact />}
-				/>
-				<Route path="/project" element={<Project />} />
-			</Routes>
-		</Router>
-		</div>
-	);
+  return (
+    <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/project">Project</Link>
+          </li>
+        </ul>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
